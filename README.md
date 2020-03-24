84b322105f8bdd5aa3deef06d7d3c075ae135b14
Isabel Durham <isabelrdurham@hotmail.com>
2020-03-23 19:47:17 -0700

    Looks up pronunciation in dictionary

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ Simple concatenative text-to-speech in Python
 [x] Stage 0: letters to sequence of 1-sec letter sounds
 [x] Stage 1: letters to sequence of properly trimmed letter sounds
 	- bonus: added spaces
-[ ] Stage 2: word to IPA to sequence of IPA sounds
+[x] Stage 2: word to ARPAbet to sequence of ARPAbet sounds
 	- pronouncing dictionary lookup
-	- likely switch to two-char codes
+	- switch to allow one-or-more-char codes
 [ ] Stage 3: ...with stress
+	- CMU pronouncing dictionary lookup
 [ ] Stage 4: ...with phonetic detail
-[ ] Stage 5: words to IPA with cross-word-boundary rules applied to IPA sounds with phonetic detail
+[ ] Stage 5: words to ARPAbet with cross-word-boundary rules applied to sounds with phonetic detail
 [ ] Stage 6: context-aware recordings/segment recordings
 
 Side goals:
--- concater.py
+++ concater.py
@@ -6,33 +6,53 @@ from pydub import AudioSegment
 quarter_rest = AudioSegment.silent(duration=150)
 half_rest = AudioSegment.silent(duration=300)
 
-def get_sound(letter):
-    """Get the sound file for a letter (/single char code)"""
-    filename = "recs/" + letter + ".wav"
-    return AudioSegment.from_file(filename, format="wav")
+class Concater:
+    def __init__(self, dict_file):
+        """Build the pronouncing dictionary"""
+        dict_text = open(dict_file, mode='r')
+        self.dictionary = {}
+        self.dictionary.update({'take':'test'})
+        for line in dict_text:
+            split = line.split('\t')
+            if len(split) < 2:
+                continue
+            self.dictionary.update({split[0]:split[1]})
+        print(self.dictionary)
+            
+    def get_sound(self, phone):
+        """Get the sound file for a letter (/single char code)"""
+        filename = "recARPAs/" + phone + ".wav"
+        return AudioSegment.from_file(filename, format="wav")
 
-def word2song(word):
-    """Return new audio segment with sound version of each letter in given word"""
-    song = []
-    song.append(half_rest)
-    for letter in word:
-        if (letter == ' '):
-            song.append(quarter_rest)
-            continue
-        if (not letter.isalnum()):
-            continue
-        song.append(get_sound(letter))
-    song.append(half_rest)
-    return sum(song)
+    def word2song(self, word):
+        """Return new audio segment with sound version of each letter in given word"""
+        song = []
+        if word not in self.dictionary:
+            print("Not in dictionary")
+            return sum(song)
+        pronunciation = self.dictionary[word]
+        for phone in pronunciation.split():
+            if (phone == ''):
+                song.append(quarter_rest)
+                continue
+            if (not phone.isalpha()):
+                continue
+            song.append(self.get_sound(phone))
+        song.append(half_rest)
+        return sum(song)
 
 def main():
     """Create and save sound file for input"""
-    word = input()
-    if (word == ''):
-        print("no word")
-        return
-    song = word2song(word)
-    song.export("songfor" + word + ".mp3", format="mp3")
+    concater = Concater("dict.txt")
+    inp = input()
+    while (inp != ''):
+        sentence = []
+        sentence.append(half_rest)
+        for word in inp.split():
+            song = concater.word2song(word)
+            sentence.append(song)
+        sum(sentence).export("songfor" + inp + ".mp3", format="mp3")
+        inp = input()
 
 if __name__ == '__main__':
     main()
